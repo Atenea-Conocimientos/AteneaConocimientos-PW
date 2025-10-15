a0c86756bc1340b02e8b02b5b4c16a39e43760d3
Juan Torres <juantor16@gmail.com>
2025-10-15 09:32:30 -0300

    Se agregan los archivos iniciales junto con el escenario de signup

---
+++ pages/paginaHome.ts
@@ -0,0 +1,20 @@
+import {Locator, Page} from '@playwright/test';
+
+export class PaginaHome {
+    readonly page: Page;
+    readonly botonCrearCuenta: Locator;
+    readonly urlHome = process.env.BASE_URL;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.botonCrearCuenta = page.getByRole('link', { name: 'Crear cuenta' });
+    }
+
+    async navegarAHome() {
+        await this.page.goto(this.urlHome!);
+    }
+    
+    async navegarARegistro() {
+        await this.botonCrearCuenta.click();
+    }
+};
---
+++ tests/registro.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+import {PaginaHome} from '../pages/paginaHome';
+import {PaginaRegistro} from '../pages/paginaRegistro';
+import {Helpers} from '../utils/helpers'
+
+let paginaHome: PaginaHome;
+let paginaRegistro: PaginaRegistro;
+let helpers: Helpers;
+
+
+dotenv.config();
+
+test.beforeEach(async ({ page }) => {
+  paginaHome = new PaginaHome(page);
+  paginaRegistro = new PaginaRegistro(page);
+  helpers = new Helpers(page)
+});
+
+
+test('TC-3: Registro de estudiante (Sign up)', async ({ page }) => {
+  const email = `estudiante${Date.now()}@automation.com`; 
+  await paginaHome.navegarAHome();
+  await paginaHome.navegarARegistro();
+  await paginaRegistro.registrarEstudiante('Juan', 'Pérez', email, 'Password123');
+  // Verificar que el registro fue exitoso
+  // Verificar que el request a /api/students/register de tipo post devuelva un 201 antes de continuar
+  await helpers.esperarPorRespuestaAPI('/api/students/register', 'POST', 201  )
+  await expect(page).toHaveURL(/.*dashboard.*/);
+  await helpers.verificarTextoVisible("Hola, Juan Pérez")
+  // Espera adicional para observar el resultado antes de cerrar el navegador
+  await page.waitForTimeout(12000);
+});
